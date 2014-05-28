24ef0781de8e84552102c6d13ff131d0e53197bc
root <dgee@uci.edu>
2014-05-28 20:45:58 +0000

    Vamsi's changes

--- ankishare/app/controllers/upload_controller.rb
+++ ankishare/app/controllers/upload_controller.rb
@@ -1,17 +1,13 @@
 class UploadController < ApplicationController
- 
-  def upload
-    uploaded_io = params[:file]
-    File.open(Rails.root.join('public/data', 'data', uploaded_io.original_filename), 'wb') do |file|
-	file.write(uploaded_io.read)
-  end
-  
-  def download
-
-
-    send_file  "#{RAILS_ROOT}/#{params[:file_name]}"
-    
-  end
-
+def index
+ 	render :file => 'app/views/upload/uploadfile.html.erb'
+end
+def uploadFile
+	post = DataFile.save(params[:upload])
+	render :text => "File has been uploaded successfully" 
+end
+def create
+	render :text => params.inspect
+	#render :text => "File has been uploaded successfully" 
 end
 end
